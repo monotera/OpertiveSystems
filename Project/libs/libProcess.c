0bd7728baa39b804af81d64afaac78e74d0c0e64
Carlos Erazo <anes_carlos@hotmail.com>
2020-10-01 15:56:50 -0500

    Resolves createMappers/mapper function

--- Project/libs/libProcess.c
+++ Project/libs/libProcess.c
@@ -57,3 +57,167 @@ void split(char *logfile, int lines, int nmappers)
         printf("Error\n");
     }
 }
+
+void createMappers(int nmappers, char *command, int intermediate){
+    int status, i;
+    pid_t childpID;
+   
+    char *splitF = (char *)malloc(5);
+    char *extension = (char *)malloc(4);
+    char *aux = (char *)malloc(10);
+    char *index = (char *)malloc(6);
+    strcpy(splitF, "split");
+    strcpy(extension, ".txt");
+    strcpy(aux, "split0.txt");
+
+
+    for(i = 0; i < nmappers; i++){
+
+        if((childpID = fork()) < 0){
+            perror("Fork: ");
+            exit(1);
+        }
+
+        mapper(aux, command, childpID, i, intermediate);
+
+        sprintf(index, "%d", i);
+        strcpy(aux, splitF);
+        strcat(aux, index);
+        strcat(aux, extension);
+    }
+
+    for(i = 0; i < nmappers ; i++){
+        wait(&status);
+    }
+}
+
+void mapper(char *split, char *command, pid_t childpID, int iter, int intermediate){
+
+    int i, count;
+    iter = 0;
+    char *pt;
+    int col = strtol(command, &pt, 10);
+    command = pt + strspn(pt, ",");
+    char prueba;
+
+    if(command[0] == '<' && command[1] == '='){
+        prueba = '?';
+        command = command + strspn(pt, ",");
+    } else if (command[0] == '>' && command[1] == '='){
+        prueba = '+';
+        command = command + strspn(pt, ",");
+    } else {
+        prueba = command[0];
+    }
+
+    char * sign = &prueba;
+    command = command + strspn(pt, ",");
+    command = command + strspn(pt, ",");
+    int value = strtol(command, &pt, 10);
+    
+
+    FILE *file = fopen(split, "r");
+    char *str = (char *)malloc(chunk);
+
+    char *splitF = (char *)malloc(5);
+    char *extension = (char *)malloc(5);
+    char *aux = (char *)malloc(10);
+    char *index = (char *)malloc(6);
+    strcpy(splitF, "buf");
+    strcpy(extension, ".txt");
+    sprintf(index, "%d", iter);
+    strcpy(aux, splitF);
+    strcat(aux, index);
+    strcat(aux, extension);
+
+
+    if(file != NULL){
+        char *str = (char *)malloc(chunk);
+
+        FILE *writer = fopen(aux, "w");
+
+        while (fgets(str, chunk, file)){        
+            count = 0;
+            int clave = 0, flag = 0;
+            for(;;){
+                count ++;
+                if(strlen(str)==1)
+                    break;
+                int cl1 = strtol(str, &pt, 10);
+                str = pt + strspn(pt, " ");
+                if(flag == 0){
+                    clave = cl1;
+                    flag = 1;
+                }
+                if(count == col){
+                    if(sign[0] == '>' && cl1 > value){
+                        fprintf(writer, "%i %s %i", clave, "\n", cl1);
+                    }
+                    if(sign[0] == '<' && cl1 < value){
+                        fprintf(writer, "%i %s %i", clave, "\n", cl1);
+                    }
+                    if(sign[0] == '+' && cl1 >= value){
+                        fprintf(writer, "%i %s %i", clave, "\n", cl1);
+                    }
+                    if(sign[0] == '?' && cl1 <= value){
+                        fprintf(writer, "%i %s %i", clave, "\n", cl1);
+                    }
+                    if(sign[0] == '=' && cl1 == value){
+                        fprintf(writer, "%i %s %i", clave, "\n", cl1);
+                    }
+                }
+
+                /*printf("%i %s",cl1, "\n");*/
+            }
+            /*break;*/
+        }
+        fclose(writer);
+        fclose(file);
+
+        if(intermediate == 0){
+            remove(split);
+            printf("%s %s", "Se ha borrado ",split);
+        }
+
+    } else {
+        perror("Error mapper: ");
+    }
+
+}
+
+void createReducers(int nreducers, int nmappers){
+    int size = nmappers/nreducers;
+
+    int status, i;
+    pid_t childpID;
+   
+    char *splitF = (char *)malloc(5);
+    char *extension = (char *)malloc(4);
+    char *aux = (char *)malloc(10);
+    char *index = (char *)malloc(6);
+    strcpy(splitF, "buf");
+    strcpy(extension, ".txt");
+    strcpy(aux, "buf0.txt");
+
+
+    for(i = 0; i < nreducers; i++){
+/*
+        if((childpID = fork()) < 0){
+            perror("Fork: ");
+            exit(1);
+        }
+
+        mapper(aux, command, childpID, i, intermediate);
+
+        sprintf(index, "%d", i);
+        strcpy(aux, splitF);
+        strcat(aux, index);
+        strcat(aux, extension);
+*/
+    }
+
+    for(i = 0; i < nmappers ; i++){
+        wait(&status);
+    }
+
+}
--- Project/libs/libProcess.h
+++ Project/libs/libProcess.h
@@ -16,9 +16,11 @@ typedef struct map
 } map;
 
 void split(char *logfile, int lines, int nmappers); /*Create split files*/
-void createMappers(int nmappers, char *command);
-void mapper(char *split, char *command);
+void createMappers(int nmappers, char *command, int intermediate);
+void mapper(char *split, char *command, pid_t childpID, int iter, int intermediate);
+
 void createBuf(map *mapper);
+
 void createReducers(int nreducers, int nmappers);
 void reducer(int intermediate); /* creates output files*/
 void printAnswer(int nreducers);
