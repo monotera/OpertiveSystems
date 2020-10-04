bd7874ecda632e5c4b9de1595da118da924139d9
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-10-03 21:48:44 -0500

    Revert "Update"
    This reverts commit 62e31ff3913c8bd304a4f7be77e8959a28ec1bc3.

--- Project/analogp.c
+++ Project/analogp.c
@@ -1,11 +1,12 @@
 #include "libs/libProcess.h"
 
 /*
-     compile file = gcc -std=c89 -o analogp analogp.c libs/libProcess.c
+     gcc -std=c89 -o analogh analogh.c libs/libProcess.c
 */
 
 int main(int argc, char *argv[])
 {
+
     if (argc != 6)
     {
         printf("Error : wrong number of parameters\n");
@@ -20,47 +21,22 @@ int main(int argc, char *argv[])
         switch (option)
         {
         case 1:
-
-            printf("Hello!, Checking for files deletions!\n");
+            printf("Hello world\n");
+            /*char * command;
+            printf("$ ");
+            scanf("%s",command);*/
             int lines = atoi(argv[2]);
-            int nmappers = atoi(argv[3]);
-            int nreducers = atoi(argv[4]);
-            int inter = atoi(argv[5]);
-            if (inter == 1)
-            {
-                clear(nmappers, nreducers);
-            }
-            printf("Done!, please insert your command line (column,sign,value)\n$");
-            char *command;
-            scanf("%s", command);
-
-            if (nreducers == 0)
-            {
-                printf("Invalid number of reducers");
-            }
-            if (inter > 1 || inter < 0)
-            {
-                printf("Error: inter must be 0 or 1");
-                exit - 1;
-            }
-            else
-            {
-
-                int output;
-                output = processControl(argv[1], lines, nmappers, nreducers, command, inter);
-                /*output = processControl("test1.txt", 534, 10, 3, "5,>=,1", 0);*/
-                if (output >= 0)
-                {
-                    printf("there are %d records that meet the condition\n", output);
-                }
-            }
+            printf("%d %s",lines,"\n");
+            int mappers = atoi(argv[3]);
+            split(argv[1],lines,mappers);
+            printf("\n");
             break;
         case 2:
             printf("Good bye world\n");
             exit(0);
             break;
         default:
-            printf("Error: Invalid selection\n");
+            printf("Invalid selection\n");
             break;
         }
     }
--- Project/libs/libProcess.h
+++ Project/libs/libProcess.h
@@ -15,22 +15,14 @@ typedef struct map
     int value;
 } map;
 
-typedef struct command
-{
-    int col;
-    int dif;
-    int eq;
-} command;
-
-int processControl(char *log, int lines, int nmappers, int nreducers, char *command, int inter);
 void split(char *logfile, int lines, int nmappers); /*Create split files*/
-void createMappers(int nmappers, char *command, int intermediate);
-void mapper(char *split, char *command, int iter, int intermediate);
+void createMappers(int nmappers, char *command);
+void mapper(char *split, char *command);
 void createBuf(map *mapper);
 void createReducers(int nreducers, int nmappers);
-int reducer(int intermediate,int * assigments, int index); /* creates output files*/
-int printAnswer(int nreducers);
-void clear();
+void reducer(int intermediate); /* creates output files*/
+void printAnswer(int nreducers);
+
 
 
 #endif
